6a40c931e492904e820df314757c6fe19f4f75b2
Umut Bektaş <info@umutbektas.com>
2019-05-23 01:06:16 +0300

    Permisson System Two

--- ebooks/models.py
+++ ebooks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
 
 
 class Ebook(models.Model):
@@ -15,7 +16,7 @@ class Ebook(models.Model):
 class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    review_author = models.CharField(max_length=8, blank=True, null=True)
+    review_author = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.TextField(blank=True, null=True)
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     ebook = models.ForeignKey(Ebook, on_delete=models.CASCADE, related_name="reviews")
